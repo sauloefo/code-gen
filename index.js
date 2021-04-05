b534c7549ffe4783b938ac7cdcbf66d3b424f253
Saulo Oliveira <me@sauloefo.com>
2021-04-05 20:02:42 +0100

    Moving settings out of codeGenSettings.js module

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { formatFilesInOutputDirectory } from "./lib/tools/formatApexFile.js";
+import cgs from "./lib/tools/codeGenSettings.js";
 import func from "./lib/domains/accessors/generateDomainAccessorsClassFile.js";
+
+cgs.loadFromFile("./templates/codeGenSettings.json");
 
 func("Case");
 formatFilesInOutputDirectory();
