501bc8a3a2f23e0fc7aabecfff06d437f60b272a
Saulo Oliveira <me@sauloefo.com>
2021-04-08 11:00:02 +0100

    Refactoring: fix generate methods naming convention & fragment results

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { formatFilesInOutputDirectory } from "./lib/tools/formatApexFile.js";
 import cgs from "./lib/tools/codeGenSettings.js";
-import func from "./lib/domains/interface/createDomainInterfaceFile.js";
+import { createDomainAccessorsClassFile as func } from "./lib/domains/accessors/createDomainAccessorsClassFile.js";
 
 cgs.loadFromFile("./templates/codeGenSettings.json");
 
