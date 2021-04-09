12216abb08903cc343abcef45da340e93f30802c
Saulo Oliveira <me@sauloefo.com>
2021-04-09 19:46:17 +0100

    Generate scaffold of traceable class.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { formatFilesInOutputDirectory } from "./lib/tools/formatApexFile.js";
 import cgs from "./lib/tools/codeGenSettings.js";
-import { createDomainAccessorsClassFile as func } from "./lib/domains/accessors/createDomainAccessorsClassFile.js";
+
+import { createTraceableRecordClassFile } from "./lib/domains/traceable-record/createTraceableRecordClassFile.js";
+import { createTraceableRecordInterfaceFile } from "./lib/domains/traceable-record-interface/createTraceableRecordInterfaceFile.js";
+
+const formatFiles = true;
+const sObjectApiName = "Case";
+const fileCreators = [
+	createTraceableRecordClassFile,
+	createTraceableRecordInterfaceFile
+]
 
 cgs.loadFromFile("./templates/codeGenSettings.json");
 
-func("Case");
-formatFilesInOutputDirectory();
+fileCreators.forEach(fileCreator => fileCreator(sObjectApiName));
+
+if (formatFiles) formatFilesInOutputDirectory();
