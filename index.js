b38219e33f4ae0b4e6e6375bf03743a3b088bed0
Saulo Oliveira <me@sauloefo.com>
2021-04-17 12:50:48 +0100

    Adding log with time elapsed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { formatFilesInOutputDirectory } from "./lib/tools/formatApexFile.js";
+import { logInfo } from "./lib/tools/logger.js";
 import cgs from "./lib/tools/codeGenSettings.js";
+
+import { createDomainAccessorsClassFile } from "./lib/domains/accessors/createDomainAccessorsClassFile.js";
+import { createDomainInterfaceFile } from "./lib/domains/interface/createDomainInterfaceFile.js";
 
 import { createTraceableRecordClassFile } from "./lib/domains/traceable-record/createTraceableRecordClassFile.js";
 import { createTraceableRecordInterfaceFile } from "./lib/domains/traceable-record-interface/createTraceableRecordInterfaceFile.js";
@@ -7,12 +11,16 @@ import { createTraceableRecordInterfaceFile } from "./lib/domains/traceable-reco
 const formatFiles = true;
 const sObjectApiName = "Case";
 const fileCreators = [
+	createDomainAccessorsClassFile,
+	createDomainInterfaceFile,
 	createTraceableRecordClassFile,
 	createTraceableRecordInterfaceFile
 ]
 
 cgs.loadFromFile("./templates/codeGenSettings.json");
 
+logInfo("Starting to create files ...");
 fileCreators.forEach(fileCreator => fileCreator(sObjectApiName));
+logInfo("Files created.");
 
 if (formatFiles) formatFilesInOutputDirectory();
